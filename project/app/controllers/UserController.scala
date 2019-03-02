de73fa14ca6583849334e2fd1f52962a6db20aef
Artur Ott <artur.ott@htwg-konstanz.de>
2019-03-02 11:04:51 +0100

    user manipulation added, crsf toke disabled

--- project/app/controllers/UserController.scala
+++ project/app/controllers/UserController.scala
@@ -1,16 +1,39 @@
 package controllers
 
 import javax.inject._
+import play.api.libs.json.{JsNumber, Json}
 import play.api.mvc._
-import repository.{Github, Parser, Firebase}
+import repository.{Firebase, Github, Parser}
 
 
 class UserController @Inject()(
     cc: ControllerComponents,
     firebase: Firebase
   ) extends AbstractController(cc) {
-  def index() = Action { implicit request: Request[AnyContent] => {
+  def index(key: String) = Action { implicit request: Request[AnyContent] => {
     val users = firebase.getUsers
+    Ok(views.html.users(key, users))
+  }}
 
+  def changeUsers(key: String, method: String) = Action { implicit request: Request[AnyContent] => {
+
+    request.body.asFormUrlEncoded match {
+      case Some(data) => {
+        val users = firebase.getUsers
+        data.map { case (id, _) => id }.foreach(id => {
+          method match {
+            case "n" => if (users(id).score >= 0) {
+              firebase.setUserData(id, Json.parse(s"""{"score": ${users(id).score * -1}}"""))
+            }
+            case "p" => if (users(id).score < 0) {
+              firebase.setUserData(id, Json.parse(s"""{"score": ${users(id).score * -1}}"""))
+            }
+          }
+        })
+      }
+      case None => ()
+    }
+
+    Redirect(controllers.routes.UserController.index(key))
   }}
 }
