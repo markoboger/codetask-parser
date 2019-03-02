577417dacc31f9679162783cfb9f173e8037f0dd
Artur Ott <artur.ott@htwg-konstanz.de>
2019-03-02 11:03:48 +0100

    exception added to the firebase repository

--- project/app/repository/Firebase.scala
+++ project/app/repository/Firebase.scala
@@ -1,11 +1,14 @@
 package repository
 
+import java.util.NoSuchElementException
+import java.util.concurrent.TimeoutException
+
 import play.api.libs.json._
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.libs.ws.{WSClient, WSRequest}
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import models.tables._
@@ -19,18 +22,32 @@ class Firebase @Inject() (config: Configuration, ws: WSClient) {
     .addHttpHeaders("Accept" -> "application/json")
     .withRequestTimeout(config.get[Int]("firebase.timeout").millis)
 
+
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  @throws(classOf[NoSuchElementException])
   private def getAllCoursesRequest: Map[String, Course] = {
     val request: WSRequest = this.createRequest(config.get[String]("firebase.tables.courses"))
-    val response: Future[Map[String, Course]] = request.get().map { values => {
-      val valuesMap: scala.collection.mutable.Map[String, Course] = new scala.collection.mutable.HashMap[String, Course]
-      values.json.as[JsObject].keys.foreach(key => {
-        valuesMap += (key -> (values.json \ key).get)
+    val response = Await.result(request.get(), config.get[Int]("firebase.timeout").millis)
+    if (response.status != 200) {
+      throw new IllegalAccessException(s"Could not get all users! (Status code is not 200)")
+    }
+    val valuesMap: scala.collection.mutable.Map[String, Course] = new scala.collection.mutable.HashMap[String, Course]
+    try {
+      response.json.as[JsObject].keys.foreach(key => {
+        valuesMap += (key -> (response.json \ key).get)
       })
-      valuesMap.toMap
-    }}
-    Await.result(response, Duration.Inf)
+    } catch {
+      case _: NoSuchElementException =>
+        throw new NoSuchElementException("Could not get all courses! (Could not parse the answer!)")
+    }
+    valuesMap.toMap
   }
 
+
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  @throws(classOf[NoSuchElementException])
   def getCourses: Map[String, Course] = {
     if (this.courses.isEmpty) {
       this.courses = this.getAllCoursesRequest
@@ -38,6 +55,10 @@ class Firebase @Inject() (config: Configuration, ws: WSClient) {
     this.courses
   }
 
+
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  @throws(classOf[NoSuchElementException])
   def getCourse(id: String) = {
     if (this.courses.isEmpty) {
       this.courses = this.getAllCoursesRequest
@@ -45,6 +66,9 @@ class Firebase @Inject() (config: Configuration, ws: WSClient) {
     this.courses(id)
   }
 
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  @throws(classOf[NoSuchElementException])
   def updateCourse(course: Course): Unit = {
     this.getCourses.find{case (id, currentCourse) => currentCourse.id == course.id} match {
       case Some((id, _)) => this.setCourseData(id, course)
@@ -53,49 +77,65 @@ class Firebase @Inject() (config: Configuration, ws: WSClient) {
         this.courses = this.getAllCoursesRequest
       }
     }
-    Some("")
   }
 
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
   def createCourse(data:JsValue): Unit = {
     val request = this.createRequest(config.get[String]("firebase.tables.courses")).post(data)
-    Await.result(request, Duration.Inf).status match {
-      case 200 => ()
-      case _ => ()
+    if (Await.result(request, config.get[Int]("firebase.timeout").millis).status != 200) {
+      throw new IllegalAccessException(s"Course could not be created! (Status code is not 200)")
     }
   }
 
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
   def setCourseData(id: String, data: JsValue, attribute: List[String] = List.empty): Unit = {
     val request = this.createRequest(config.get[String]("firebase.tables.courses"), List(id) ++ attribute).patch(data)
-    Await.result(request, Duration.Inf).status match {
-      case 200 => ()
-      case _ => ()
+    if (Await.result(request, config.get[Int]("firebase.timeout").millis).status != 200) {
+      throw new IllegalAccessException(s"Course (${id}) could not be set! (Status code is not 200)")
     }
   }
 
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  @throws(classOf[NoSuchElementException])
   def getUsers: Map[String, User] = {
     val request: WSRequest = this.createRequest(config.get[String]("firebase.tables.users"))
-    val response: Future[Map[String, User]] = request.get().map { values => {
-      val valuesMap: scala.collection.mutable.Map[String, User] = new scala.collection.mutable.HashMap[String, User]
-      values.json.as[JsObject].keys.foreach(key => {
-        valuesMap += (key -> (values.json \ key).get)
+    val response = Await.result(request.get(), config.get[Int]("firebase.timeout").millis)
+    if (response.status != 200) {
+      throw new IllegalAccessException(s"Could not get all users! (Status code is not 200)")
+    }
+    val valuesMap: scala.collection.mutable.Map[String, User] = new scala.collection.mutable.HashMap[String, User]
+    try {
+      response.json.as[JsObject].keys.foreach(key => {
+        valuesMap += (key -> (response.json \ key).get)
       })
-      valuesMap.toMap
-    }}
-    Await.result(response, Duration.Inf)
+    } catch {
+      case _: NoSuchElementException =>
+        throw new NoSuchElementException("Could not get all users! (Could not parse the answer!)")
+    }
+    valuesMap.toMap
   }
 
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
   def getUser(id: String): User = {
-    val response: Future[User] = this.createRequest(config.get[String]("firebase.tables.users"), List(id))
+    val response = this.createRequest(config.get[String]("firebase.tables.users"), List(id))
       .get()
-      .map {_.json}
-    Await.result(response, Duration.Inf)
+    val result = Await.result(response, config.get[Int]("firebase.timeout").millis)
+    if (result.status != 200) {
+      throw new IllegalAccessException(s"Could not get user (${id})! (Status code is not 200)")
+    }
+    result.json
   }
 
-  def setUserData(id: String, data: JsValue, attribute: List[String] = List.empty): Boolean = {
+  @throws(classOf[TimeoutException])
+  @throws(classOf[IllegalAccessException])
+  def setUserData(id: String, data: JsValue, attribute: List[String] = List.empty): Unit = {
     val request = this.createRequest(config.get[String]("firebase.tables.users"), List(id) ++ attribute).patch(data)
-    Await.result(request, Duration.Inf).status match {
-      case 200 => true
-      case _ => false
+    if (Await.result(request, config.get[Int]("firebase.timeout").millis).status != 200) {
+      throw new IllegalAccessException(s"User (${id}) could not be set! (Status code is not 200)")
     }
   }
 }
